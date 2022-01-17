0ff3e9f0e27f90c6d92bbba5591014be2933c89a
Ruchira Sachinthana <ruchira.sachinthana.1@gmail.com>
2022-01-17 10:49:34 +0530

    updated

--- contact-us.php
+++ contact-us.php
@@ -11,10 +11,18 @@ if(isset($_POST['submit']) ) {
     $email_body = "Message from Contact Us page of the Website: <br>";
     $email_body .= "<b>From:</b> {$fullname} <br>";
     $email_body .= "<b>Subject:</b> {$subject} <br>";
-    $email_body .= "<b>Message:</b> {$message} <br>";
+    $email_body .= "<b>Message:</b><br>" . nl2br(strip_tags($message));
 
 
-    mail()
+    $header = "From: {$email}\r\nContent-Type: text/html;";
+
+    $send_mail_result = mail($to, $mail_subject, $email_body, $header);
+
+    if($send_mail_result) {
+        echo "Message Sent.";
+    } else{
+        echo "Message not sent!";
+    }
 }
 
 
